a8baceb2b85fdf71bf0f209694899193b4e3d875
Ealian Kowalsky <ealiank@icloud.com>
2021-03-24 19:50:35 -0600

    Adding a classification for how old you are

--- greet.rb
+++ greet.rb
@@ -3,6 +3,15 @@
 print "What is your name? "
 your_name = gets
 puts "Hello #{your_name}"
-print "What is your age?"
-your_age = gets
+print "What is your age? "
+your_age = gets.to_i
 puts "you are #{your_age} years old."
+if (0..3).include? your_age
+  puts "You are a baby"
+elsif (4..12).include? your_age
+  puts "You are a kid"
+elsif (13..19).include? your_age
+  puts "You are a teen"
+else
+  puts "You are an adult"
+end
